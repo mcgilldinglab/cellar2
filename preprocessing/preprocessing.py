3e8cf4e018db0587f74aa5d64c0bfcfc94106f96
Minzhe Feng <75942143+mfffeng@users.noreply.github.com>
2022-06-19 02:00:37 -0400

    Add basic mode switching

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -43,22 +43,23 @@ class Preprocessor():
             self.matrix,                             # the directory with the `.mtx` file
             var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
             cache=True)                              # write a cache file for faster subsequent reading
-        adata.var_names_make_unique()       # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
-        sc.pp.filter_cells(adata, min_genes=200)
-        sc.pp.filter_genes(adata, min_cells=3)
-        adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-        sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-        adata = adata[adata.obs.n_genes_by_counts < 2500, :]
-        adata = adata[adata.obs.pct_counts_mt < 5, :]
-        sc.pp.normalize_total(adata, target_sum=1e4)
-        sc.pp.log1p(adata)
-        sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-        adata = adata[:, adata.var.highly_variable]
-        sc.pp.scale(adata, max_value=10)
-        adata.write('/mnt/data/cellar/preprocessed/pbmc_1k_v3_filtered.h5ad')
+        if self.mode == 'rna_seq':
+            adata.var_names_make_unique()       # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
+            sc.pp.filter_cells(adata, min_genes=200)
+            sc.pp.filter_genes(adata, min_cells=3)
+            adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+            sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+            adata = adata[adata.obs.n_genes_by_counts < 2500, :]
+            adata = adata[adata.obs.pct_counts_mt < 5, :]
+            sc.pp.normalize_total(adata, target_sum=1e4)
+            sc.pp.log1p(adata)
+            sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+            adata = adata[:, adata.var.highly_variable]
+            sc.pp.scale(adata, max_value=10)
+            adata.write('/mnt/data/cellar/preprocessed/pbmc_1k_v3_filtered.h5ad')
 
 
-preprocessor = Preprocessor('/mnt/data/cellar/data/raw/pbmc_1k_v3_fastqs.tar', 'dummy')
+preprocessor = Preprocessor(raw='/mnt/data/cellar/data/raw/pbmc_1k_v3_fastqs.tar', mode='rna_seq')
 # TODO: Choose reference data to use according to the kind of raw data
 preprocessor.count('/mnt/data/cellar/data/ref/refdata-cellranger-GRCh38-3.0.0')
 preprocessor.read()
