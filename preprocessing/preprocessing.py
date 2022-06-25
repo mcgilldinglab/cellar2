8bd469674fc5500bd9e79ae0d93c36344f10f7ab
Minzhe Feng <75942143+mfffeng@users.noreply.github.com>
2022-06-25 13:24:04 -0400

    Add preprocessing for spatial data

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -6,33 +6,52 @@ import scanpy as sc
 
 class Preprocessor():
     def __init__(self, raw: str, mode: str) -> None:
+        self.base_dir = re.match(r'^([^.]*)\..+$', raw.split('/')[-1]).group(1)
         self.raw = raw
         self.mode = mode
         self.matrix = None
 
-    def count(self, ref_dir: str) -> None:
+    def count(self, ref_dir: str, probe_dir: str=None, image_dir: str=None) -> None:
         # Working directory is /mnt/data/cellar
         # User uploaded raw fastq data sits in /data/raw
         # Prepared reference data sits in /data/ref
         # Temporary unpack location is /data/tmp
-        base_name = self.raw.split('/')[-1]
         tmp_location = '/mnt/data/cellar/data/tmp'
         subprocess.run(['mkdir', tmp_location])
         try:
-            subprocess.run(['tar', '-xvf', self.raw, '-C', tmp_location], check=True, capture_output=True)
+            subprocess.run(['tar', '-xvf', self.raw, '-C', tmp_location], check=True)
         except subprocess.CalledProcessError as e:
             print(f'Cannot extract tar file, error:\n {e.stderr}')
             raise e
         try:
             # Extract the base name of the tar file
-            base_dir = re.match(r'^([^.]*)\..+$', base_name).group(1)
-            subprocess.run(['cellranger', 'count', f'--id=run_count_{base_dir}',
-            f'--fastqs={tmp_location}/{base_dir}',
-            # TODO: Implement sample selection with "--sample"
-            f'--transcriptome={ref_dir}'], check=True, capture_output=True)
-            self.matrix = f'/mnt/data/cellar/run_count_{base_dir}/outs/filtered_feature_bc_matrix/'
+            if self.mode == 'rna_seq':
+                subprocess.run(['cellranger', 'count', f'--id=output_{self.base_dir}',
+                f'--fastqs={tmp_location}/{self.base_dir}',
+                # TODO: Implement sample selection with "--sample"
+                f'--transcriptome={ref_dir}'], check=True, capture_output=True)
+                self.matrix = f'/mnt/data/cellar/output_{self.base_dir}/outs/filtered_feature_bc_matrix/'
+            elif self.mode == 'spatial_ffpe':
+                subprocess.run(['spaceranger', 'count', f'--id=output_{self.base_dir}',
+                f'--transcriptome={ref_dir}',
+                f'--probe-set={probe_dir}',
+                f'--fastqs={tmp_location}/{self.base_dir}',
+                f'--image={image_dir}',
+                # TODO: Find a way to get `slide` and `area` either from the raw data or from the user input
+                f'--slide=V10L13-021',
+                f'--area=B1', '--reorient-images'], check=True)
+                self.matrix = f'/mnt/data/cellar/output_{self.base_dir}/outs/filtered_feature_bc_matrix/'
+            elif self.mode == 'spatial_ff':
+                subprocess.run(['spaceranger', 'count', f'--id=output_{self.base_dir}',
+                f'--transcriptome={ref_dir}',
+                f'--fastqs={tmp_location}/{self.base_dir}',
+                f'--image={image_dir}',
+                # TODO: Find a way to get `slide` and `area` either from the raw data or from the user input
+                f'--slide=V10N30-322',
+                f'--area=A1'], check=True, capture_output=True)
+                self.matrix = f'/mnt/data/cellar/output_{self.base_dir}/outs/filtered_feature_bc_matrix/'
         except subprocess.CalledProcessError as e:
-            print(f'Cannot make fastq, error:\n {e.stderr}')
+            print(f'Cannot run count pipeline, error:\n {e.stderr}')
             raise e
         subprocess.run(['rm', '-rf', tmp_location])
 
@@ -49,17 +68,34 @@ class Preprocessor():
             sc.pp.filter_genes(adata, min_cells=3)
             adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
             sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-            adata = adata[adata.obs.n_genes_by_counts < 2500, :]
-            adata = adata[adata.obs.pct_counts_mt < 5, :]
+            adata = adata[adata.obs.pct_counts_mt < 20, :]
             sc.pp.normalize_total(adata, target_sum=1e4)
             sc.pp.log1p(adata)
             sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
             adata = adata[:, adata.var.highly_variable]
             sc.pp.scale(adata, max_value=10)
-            adata.write('/mnt/data/cellar/preprocessed/pbmc_1k_v3_filtered.h5ad')
+            adata.write(f'/mnt/data/cellar/preprocessed/{self.base_dir}.h5ad')
+        elif self.mode == 'spatial_ffpe' or self.mode == 'spatial_ff':
+            adata.var_names_make_unique()
+            adata.var["mt"] = adata.var_names.str.startswith("MT-")
+            sc.pp.calculate_qc_metrics(adata, qc_vars=["mt"], inplace=True)
+            sc.pp.filter_cells(adata, min_counts=5000)
+            sc.pp.filter_cells(adata, max_counts=35000)
+            adata = adata[adata.obs["pct_counts_mt"] < 20]
+            sc.pp.filter_genes(adata, min_cells=10)
+            sc.pp.normalize_total(adata, inplace=True)
+            sc.pp.log1p(adata)
+            sc.pp.highly_variable_genes(adata, flavor="seurat", n_top_genes=2000)
+            # TODO: Change output file name
+            adata.write(f'/mnt/data/cellar/preprocessed/{self.base_dir}.h5ad')
+        elif self.mode == 'codex':
+            pass
 
 
-preprocessor = Preprocessor(raw='/mnt/data/cellar/data/raw/pbmc_1k_v3_fastqs.tar', mode='rna_seq')
+
+preprocessor = Preprocessor(raw='/mnt/data/cellar/data/raw/Visium_FFPE_Human_Intestinal_Cancer_fastqs.tar', mode='spatial_ffpe')
 # TODO: Choose reference data to use according to the kind of raw data
-preprocessor.count('/mnt/data/cellar/data/ref/refdata-cellranger-GRCh38-3.0.0')
+preprocessor.count(ref_dir='/mnt/data/cellar/data/ref/refdata-cellranger-spaceranger-human',
+                   probe_dir='/mnt/data/cellar/data/raw/Visium_FFPE_Human_Intestinal_Cancer_probe_set.csv',
+                   image_dir='/mnt/data/cellar/data/raw/Visium_FFPE_Human_Intestinal_Cancer_image.jpg',)
 preprocessor.read()
